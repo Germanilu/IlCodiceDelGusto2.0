99d5b7bbf67a45c129aed9172d04ef469cd22722
Luciano Germani <lucianogermani@hotmail.com>
2024-07-10 17:51:28 +0200

    fix: build error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
+const nextConfig = {
+    output: 'standalone',
+}
 
 const withNextIntl = require('next-intl/plugin')(
     './i18n.js'
