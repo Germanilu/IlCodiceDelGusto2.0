4bf290499b85a7c8ece7178561b0ddaad892ccea
Luciano Germani <lucianogermani@hotmail.com>
2024-07-12 10:02:34 +0200

    Feat: try cookiebot service

--- src/app/[locale]/components/button/index.jsx
+++ src/app/[locale]/components/button/index.jsx
@@ -14,14 +14,14 @@ const Button = ({ href }) => {
   const t = useTranslations("Button");
 
   return (
-    <div class="container">
+    <div className="container">
       <Link href={`${href ? href : '/'}`}>
-        <div class="button type--A">
-          <div class="button__line"></div>
-          <div class="button__line"></div>
-          <span class="button__text">{t('back')}</span>
-          <div class="button__drow1"></div>
-          <div class="button__drow2"></div>
+        <div className="button type--A">
+          <div className="button__line"></div>
+          <div className="button__line"></div>
+          <span className="button__text">{t('back')}</span>
+          <div className="button__drow1"></div>
+          <div className="button__drow2"></div>
         </div>
       </Link>
     </div>
