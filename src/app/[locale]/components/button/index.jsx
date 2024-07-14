8083afda38b174dae5d6d98fc7ec97eb719996c4
Luciano Germani <lucianogermani@hotmail.com>
2024-07-14 09:38:54 +0200

    Fix: add desc to button component

--- src/app/[locale]/components/button/index.jsx
+++ src/app/[locale]/components/button/index.jsx
@@ -2,6 +2,13 @@ import {Link}               from '@/navigation';
 import { useTranslations }  from "next-intl";
 import './index.scss';
 
+/**
+ * @component Button
+ * <Button text={'contact-us'} href={'/contact-us'}/>
+ * 
+ * @param {String} text 
+ * @param {String} href 
+ */
 const Button = ({text, href}) => {
   const t     = useTranslations("Navbar");
 
