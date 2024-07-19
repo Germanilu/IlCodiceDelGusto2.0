2f7b675a95f288f47a0f87789e89052c7a7c389c
Luciano Germani <lucianogermani@hotmail.com>
2024-07-19 13:16:25 +0200

    Fix translations contact page

--- src/app/[locale]/contact-us/page.jsx
+++ src/app/[locale]/contact-us/page.jsx
@@ -2,6 +2,13 @@ import { useTranslations }  from "next-intl";
 import Contact              from '../components/contact/index';
 import './page.scss'
 
+
+export const metadata = {
+  title: "IlCodiceDelGusto.com",
+  description: "Ti aiutiamo a creare il miglior sito web per il tuo ristorante ",
+  keywords: "realizzazione siti web,siti web per ristoranti, siti ristoranti, sito web per ristorante, ristorante sito web",
+}
+
 export default function ContactUs(){
   const t = useTranslations("Contact-us");
   return (
@@ -9,25 +16,24 @@ export default function ContactUs(){
       <h1 className="title">{t('title')}</h1>
       <div className="container-image">
         <div className="contact-container">
-          
+
         </div>
       </div>
       <div className="container-text">
         <div className="profile-data">
-          <h2 className="name">LUCIANO GERMANI</h2>
-          <h3 className="profession">Full Stack Developer</h3>
-          <span className="desc">Ciao a tutti! Sono Luciano, un appassionato sviluppatore Full Stack con un talento per creare esperienze digitali innovative. 
-          Il mio viaggio nel mondo della programmazione è stato un'avventura emozionante, e sono entusiasta di condividere con te un assaggio di questa esperienza.</span>
+          <h2 className="name">{t('name')}</h2>
+          <h3 className="profession">{t('profession')}</h3>
+          <span className="desc">{t('desc')}</span>
         </div>
         <div className="services-data">
-          <h3>Consulenza Web</h3>
-          <p>Consulenza web | Consulenza social</p>
-          <h3>Sviluppo Web</h3>
-          <p>Biglietti da visita virtuali | Menù digitali con qr code| Siti web vetrina semplici | Siti web a pagina singola | Siti web con più pagine | Siti web con più lingue | Rifacimento siti web | Miglioramento siti web | Landing page | Applicazioni web | Gestionali web | Ottimizzazioni web | Ottimizzazioni seo</p>
-          <h3>Marketing Web</h3>
-          <p>Marketing Web | Marketing Email | Marketing social</p>
-          <h3>Web Design</h3>
-          <p>Depliant e brochures | Biglietti da visita | Menú digitale</p>
+          <h3>{t('h1')}</h3>
+          <p>{t('p1')}</p>
+          <h3>{t('h2')}</h3>
+          <p>{t('p2')}</p>
+          <h3>{t('h3')}</h3>
+          <p>{t('p3')}</p>
+          <h3>{t('h4')}</h3>
+          <p>{t('p4')}</p>
         </div>
         
       </div>
