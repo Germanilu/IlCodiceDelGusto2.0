a5fc5b17cb817a7cfb1aaea2a5548efe78136522
Luciano Germani <lucianogermani@hotmail.com>
2024-07-21 17:47:20 +0200

    Fix: page sections titles and qr code mobile

--- src/app/[locale]/components/qr-code/index.jsx
+++ src/app/[locale]/components/qr-code/index.jsx
@@ -1,17 +1,22 @@
+'use client'
 import { useTranslations } from "next-intl";
+import { useSelector }      from 'react-redux';
 import Image from 'next/image';
 import qrCode from '../../../../static/media/svg/qr-code.svg';
+import qrCodeMobile from '../../../../static/media/svg/qr-code-mobile.svg';
 import './index.scss';
 
 const QrCode = () => {
+
   const t = useTranslations("Qr-code");
+  const isMobile = useSelector(state => state.responsive.isMobile);
 
   return (
     <div className='qr-code-design'>
       <h2 className='title'>{t('title')}</h2>
       <div className="qr-code-container">
         <div className="image-container">
-          <Image className='image' src={qrCode} alt={'qr-code'} />
+          <Image className='image' src={isMobile ? qrCodeMobile : qrCode} alt={'qr-code'} />
         </div>
         <div className="text-container">
           <h2 className='container-title'>{t('subtitle')}</h2>
