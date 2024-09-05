6cb5ba401be59388ef6df1d6a2ac3c1411de7345
Luciano Germani <lucianogermani93@gmail.com>
2024-09-05 16:00:27 +0200

    Add 2 project

--- src/settings/projects/index.js
+++ src/settings/projects/index.js
@@ -1,8 +1,9 @@
 const projects = [
     { id: 1, title: "Tokio",className:"img-one", href:"https://violet-rhinoceros-mp4p3rblkxi0bjpa.builder-preview.com/" },
     { id: 2, title: "Sazón",className:"img-two", href:"https://plantilla-1.vercel.app/es" },
-    { id: 3, title: "Veggie Bites",className:"img-three", href:"https://germanilu.github.io/Veggie-Bites/" },
-    { id: 3, title: "Sapore di Natura",className:"img-four", href:"https://steelblue-snail-361751.hostingersite.com/" },
+    { id: 3, title: "Veggie Bites",className:"img-three", href:"https://veggiebites2-0-2vzc.vercel.app/" },
+    { id: 4, title: "Sapore di Natura",className:"img-four", href:"https://steelblue-snail-361751.hostingersite.com/" },
+    { id: 5, title: "Savana",className:"img-five", href:"https://plantilla-2-l4rj.vercel.app/" },
 ];
 
 export default projects
