4c9debc4e3b9756c617ed0e567f83577570b34be
Luciano Germani <lucianogermani93@gmail.com>
2024-11-18 12:08:21 +0100

    Feat: new project add

--- src/settings/projects/index.js
+++ src/settings/projects/index.js
@@ -3,9 +3,10 @@ const projects = [
     { id: 2, title: "Paucek",className:"img-seven", href:"https://yellowgreen-rat-976997.hostingersite.com/" },
     { id: 3, title: "Savana",className:"img-five", href:"https://plantilla-2-l4rj.vercel.app/" },
     { id: 4, title: "Veggie Bites",className:"img-three", href:"https://veggiebites2-0-2vzc.vercel.app/" },
-    { id: 5, title: "Sazón",className:"img-two", href:"https://plantilla-1.vercel.app/es" },
-    { id: 6, title: "The Lab",className:"img-six", href:"https://plantilla-3-tf79.vercel.app/" },
-    { id: 7, title: "Sapore di Natura",className:"img-four", href:"https://steelblue-snail-361751.hostingersite.com/" }
+    { id: 5, title: "Forno e Fiamma",className:"img-eight", href:"https://tan-monkey-915322.hostingersite.com/" },
+    { id: 6, title: "Sazón",className:"img-two", href:"https://plantilla-1.vercel.app/es" },
+    { id: 7, title: "The Lab",className:"img-six", href:"https://plantilla-3-tf79.vercel.app/" },
+    { id: 8, title: "Sapore di Natura",className:"img-four", href:"https://steelblue-snail-361751.hostingersite.com/" },
 ];
 
 export default projects
