229a95963d4f0fe6ac45b72dee88fe444fc73d64
Luciano Germani <lucianogermani93@gmail.com>
2024-11-21 13:37:15 +0100

    Feat: new proj

--- src/settings/projects/index.js
+++ src/settings/projects/index.js
@@ -2,11 +2,12 @@ const projects = [
     { id: 1, title: "Tokio",className:"img-one", href:"https://violet-rhinoceros-mp4p3rblkxi0bjpa.builder-preview.com/" },
     { id: 2, title: "Paucek",className:"img-seven", href:"https://yellowgreen-rat-976997.hostingersite.com/" },
     { id: 3, title: "Savana",className:"img-five", href:"https://plantilla-2-l4rj.vercel.app/" },
-    { id: 4, title: "Veggie Bites",className:"img-three", href:"https://veggiebites2-0-2vzc.vercel.app/" },
+    { id: 4, title: "Sovy",className:"img-nine", href:"https://lightcoral-lyrebird-521538.hostingersite.com/" },
     { id: 5, title: "Forno e Fiamma",className:"img-eight", href:"https://tan-monkey-915322.hostingersite.com/" },
     { id: 6, title: "Sazón",className:"img-two", href:"https://plantilla-1.vercel.app/es" },
     { id: 7, title: "The Lab",className:"img-six", href:"https://plantilla-3-tf79.vercel.app/" },
     { id: 8, title: "Sapore di Natura",className:"img-four", href:"https://steelblue-snail-361751.hostingersite.com/" },
+    { id: 9, title: "Veggie Bites",className:"img-three", href:"https://veggiebites2-0-2vzc.vercel.app/" },
 ];
 
 export default projects
