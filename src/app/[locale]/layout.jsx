6f74b8c05c0a9519311d725e33f727999536d5a0
Luciano Germani <lucianogermani93@gmail.com>
2024-11-27 14:12:35 +0100

    Feat: meta pixel

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -35,6 +35,10 @@ export default function RootLayout({ children, params }) {
     notFound();
   }
 
+  const META_PIXEL_ID = '1597376557819887';
+
+
+
   return (
     <html lang={locale}>
       <body className={inter.className}>
@@ -45,13 +49,42 @@ export default function RootLayout({ children, params }) {
             {/* google analytcs */}
             <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9RX88331DS"></Script>
             <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-9RX88331DS');
-        `}
-      </Script>
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-9RX88331DS');
+              `}
+            </Script> 
+        
+            {/* Meta pixel ID */}
+            <Script id="meta-pixel" strategy="afterInteractive">
+              {`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${META_PIXEL_ID}');
+                fbq('track', 'PageView');
+              `}
+            </Script>
+
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: 'none' }}
+                src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+              />
+            </noscript>
+
+
+          
+
             {/* <Script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="4b888c1a-2a74-4132-8f39-674f4881c80b" data-blockingmode="auto" type="text/javascript"></Script> */}
             <Navbar />
             <ScrollToTop />
